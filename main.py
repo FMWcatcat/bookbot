0862e823e9cd657edbf11910125d66335ce7f1f2
FMWcatcat <nikomakela15@gmail.com>
2025-02-19 07:11:11 +0200

    update2

---
+++ main.py
@@ -0,0 +1,30 @@
+def main():
+    
+    with open("books/frankenstein.txt") as f:
+        print("--- Begin report of books/frankenstein.txt ---")
+        file_contents = f.read()
+
+        word_count = len(file_contents.split())
+        print(f"{word_count} words found in the document")
+
+        
+        char_count = ({})
+        #keys = char_count.keys()
+        #values = char_count.values()
+        for c in file_contents.lower():
+            if c.isalpha():
+                if c in char_count:
+                    char_count [c] += 1
+                else:
+                    char_count [c] = 1
+        def sort_on(char_count):
+            return char_count[nums]
+        
+        
+        print (f"The '{key in char_count}' character was found {value in char_count} times")
+        
+
+        
+
+        
+main()
